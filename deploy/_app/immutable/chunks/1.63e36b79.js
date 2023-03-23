15feecc7e09e4e2301b55d4d03d082deca7b1cbe
tejes <tmank14319@gmail.com>
2023-03-23 17:12:51 +0530

    some adapter changes

---
+++ deploy/_app/immutable/chunks/1.63e36b79.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.7db2ac27.js";export{t as component};
---
+++ deploy/_app/immutable/chunks/2.9fec300e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.56227312.js";export{t as component};
